d616a4c76e23ff82f30d7cd7950b886123a14b44
Sancho <sashabos200424@gmail.com>
2023-12-26 00:01:59 +0200

    lab2

--- WebApplication1/Dto/CompanyForCreationDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Dto
-{
-    public class CompanyForCreationDto
-    {
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public string Country { get; set; }
-    }
-}
